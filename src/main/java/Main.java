ceae87cd604a784ffae259177fc186d22f6c625b
Guillaume <guillaumexue@outlook.fr>
2023-01-24 14:41:05 +0100

    Debut du projet

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class Main {
+
+    private Menu menu;
+
+    public Main() {
+        this.menu = new Menu();
+        this.menu.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+
+}
