0926b0413bf1c4ba29685f82e2eb098230b7ad96
Guillaume <guillaumexue@outlook.fr>
2023-01-24 14:52:47 +0100

    Debut du projet

---
+++ src/main/java/Menu.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import javax.swing.*;
+
+public class Menu extends JFrame {
+
+    public Menu() {
+
+        this.setTitle("Labyrinthe");
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+
+}
