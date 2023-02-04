875c39708c9964a9beaf6ca099f51623bc842ad5
ROBIC matthieu <matth.r03@gmail.com>
2023-02-04 20:08:12 +0100

    maj

--- src/main/java/model/Micro_Model.java
+++ src/main/java/model/Micro_Model.java
@@ -16,7 +16,7 @@ public class Micro_Model {
 	AudioFormat getAudioFormat() {
 		float sampleRate = 16000;
 		int sampleSizeInBits = 8;
-		int channels = 2;
+		int channels = 1;
 		boolean signed = true;
 		boolean bigEndian = true;
 		AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,channels, signed, bigEndian);			
