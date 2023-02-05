9a48e282d79d7e18aa890b7119876201e2f2bc86
Guillaume <guillaumexue@outlook.fr>
2023-02-05 16:20:11 +0100

    Amelioration esthétique du Labyrinthe version1.

--- src/main/java/gui/Labyrinthe_GUI.java
+++ src/main/java/gui/Labyrinthe_GUI.java
@@ -1,9 +1,9 @@
 package main.java.gui;
 
 import main.java.model.Labyrinthe;
-
 import javax.swing.*;
 import java.awt.*;
+
 
 public class Labyrinthe_GUI extends JFrame {
 
@@ -12,7 +12,7 @@ public class Labyrinthe_GUI extends JFrame {
 
     public Labyrinthe_GUI(){
         this.setTitle("Labyrinthe");
-        this.setSize(1280, 720);
+        this.setSize(1366, 768);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
 
@@ -29,14 +29,50 @@ public class Labyrinthe_GUI extends JFrame {
             remplir();
         }
 
+        public ImageIcon resizeImg(String paths){
+            ImageIcon imageIcon = new ImageIcon(paths);
+            Image img = imageIcon.getImage();
+            Image imageScal = img.getScaledInstance(40,40,Image.SCALE_DEFAULT);
+            return new ImageIcon(imageScal);
+        }
+
+        public JLabel getTree(int i, int j){
+            ImageIcon arbre1 = resizeImg("src/resources/Arbre1.png");
+            ImageIcon arbre2 = resizeImg("src/resources/Arbre2.png");
+            ImageIcon arbre3 = resizeImg("src/resources/Arbre3.png");
+
+            JLabel image = new JLabel();
+
+            if(i < labyrinthe.getX()-2){
+                if(labyrinthe.getVal(i+1,j) == 0 && labyrinthe.getVal(i+2,j) == 0){
+                    image.setIcon(arbre3);
+                }else if(labyrinthe.getVal(i+1,j) == 1){
+                    image.setIcon(arbre1);
+                }else if(labyrinthe.getVal(i+1,j) == 0){
+                    image.setIcon(arbre2);
+                }
+            }else{
+                image.setIcon(arbre3);
+            }
+            return image;
+        }
+
+        public JLabel getFloor(){
+            JLabel image = new JLabel();
+            ImageIcon sol = resizeImg("src/resources/Galerie.png");
+            image.setIcon(sol);
+            return image;
+        }
+
         public void remplir(){
+
             for(int i=0;i<labyrinthe.getX();i++){
                 for(int j=0;j<labyrinthe.getY();j++){
                     JPanel tmp = new JPanel();
+                    tmp.setLayout(new BorderLayout());
                     switch (labyrinthe.getVal(i,j)){
-                        case 0 : tmp.setBackground(Color.black); break;
-                        case 1 : tmp.setBackground(Color.white); break;
-                        case 2 : tmp.setBackground(Color.gray); break;
+                        case 0 : tmp.add(getTree(i,j)); break;
+                        case 1 : tmp.add(getFloor()); break;
                     }
                     this.add(tmp);
                 }
