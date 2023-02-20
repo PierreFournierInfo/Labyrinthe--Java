1cc137e0e572078fd10ea41bc7eae012cf797d49
ROBIC matthieu <matth.r03@gmail.com>
2023-02-20 18:55:45 +0100

    maj fonction qui renvoie un tableau avec le nombre d'hommes et femmes (à tester pour être sûr)

--- src/main/java/model/Analyse_audio.java
+++ src/main/java/model/Analyse_audio.java
@@ -4,14 +4,14 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Analyse_audio {
-	
+
 	public int nbrLocuteur() {
 		int nbrLoc = 0;
 		try {
 			FileInputStream file = new FileInputStream("./src/resources/RecordAudio.txt"); //Là où est normalement enregistré le fichier généré par LIUM
 			Scanner sc = new Scanner(file);		
 			while(sc.hasNextLine()) {
-				if(sc.nextLine().charAt(0) == ';') { //chaque nouveau segment avec locuteur commence par ";" (c'est quand il y a marqué "cluster" dans le fichier .txt que ça indique un nouveau locuteur.)
+				if(sc.nextLine().charAt(0) == ';') { //chaque nouveau segment avec locuteur commence par ";"
 					nbrLoc++;
 				}
 			}
@@ -22,9 +22,38 @@ public class Analyse_audio {
 		}
 		return nbrLoc;
 	}
-	
+
+	public int[] nbrHommesFemmes() {
+		int [] hommesFemmes = new int[2]; //case 0 correspond au nombre d'hommes, case 1 correspond au nombre de femmes	
+		String s = " ";
+		try {
+			FileInputStream file = new FileInputStream("./src/resources/RecordAudio.txt");
+			Scanner sc = new Scanner(file);		
+			while(sc.hasNextLine() && sc.hasNext()) {
+				s = sc.nextLine();
+				if(s.charAt(0) == ';') {	
+					s = sc.nextLine();
+					for(int i = 0; i<s.length(); ++i) { //dans la ligne la première lettre majuscule correspond au genre (voir issue "Lire le site suivant")
+						if(s.charAt(i) == 'F') { 
+							hommesFemmes[1]++;
+						}
+						if(s.charAt(i) == 'M')  {
+							hommesFemmes[0]++;
+						}
+					}				
+				}
+			}
+			sc.close();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return hommesFemmes;
+	}
+
 	public static void main(String[] args) {
 		Analyse_audio a = new Analyse_audio();
 		System.out.println(a.nbrLocuteur());
+		System.out.println(a.nbrHommesFemmes()[0] + " " + a.nbrHommesFemmes()[1]);
 	}
 }
