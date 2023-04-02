6447aa19f296a90dcff20b5f277fd60a31813a12
ROBIC matthieu <matth.r03@gmail.com>
2023-04-02 18:06:31 +0200

    Script qui lance le programme de transcription de fichier audio en txt

--- Direction.sh
+++ Direction.sh
@@ -1,3 +1,4 @@
+#Site pour pour tout installer : https://github.com/openai/whisper (suivre les commandes dans le README)
 #!/usr/bash
 /usr/bin/java .local/bin/whisper RecordAudio.wav --language French --model base
 
