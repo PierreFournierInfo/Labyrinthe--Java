9b0f8a209110adb4465f0cd8269ebe24496daedd
ROBIC matthieu <matth.r03@gmail.com>
2023-04-08 17:04:36 +0200

    Delete Direction.sh

--- Direction.sh
+++
@@ -1,4 +0,0 @@
-#Site pour pour tout installer : https://github.com/openai/whisper (suivre les commandes dans le README)
-#!/usr/bash
-/usr/bin/java ~/.local/bin/whisper src/resources/Audio/RecordAudio.wav --language French --model base
-
