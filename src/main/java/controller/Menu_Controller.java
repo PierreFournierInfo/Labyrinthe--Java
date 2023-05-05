801c189822a3f1f6c274f085892edebf917c0dad
Guillaume <guillaumexue@outlook.fr>
2023-05-05 17:54:36 +0200

    Ajout de map et suppression du textArea de menu.

--- src/main/java/controller/Menu_Controller.java
+++ src/main/java/controller/Menu_Controller.java
@@ -3,9 +3,6 @@ package main.java.controller;
 import main.java.gui.Labyrinthe.Labyrinthe_Launcher;
 import main.java.gui.Menu.Menu;
 
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-
 public class Menu_Controller {
 
     private Menu menu;
@@ -15,7 +12,6 @@ public class Menu_Controller {
         this.menu = menu;
         this.launchLabyrinthe();
         this.launchLium();
-        this.getNbPlayer();
     }
 
     public void launchLabyrinthe(){
@@ -31,24 +27,6 @@ public class Menu_Controller {
                 this.labyrintheJeu = new Labyrinthe_Launcher(this.menu ,true);
                 labyrintheJeu.setVisible(true);
         });
-    }
-
-    public void getNbPlayer(){
-        menu.getText().addFocusListener(new FocusListener() {
-            public void focusGained(FocusEvent e) {
-                menu.getText().setText("");
-            }
-
-            public void focusLost(FocusEvent e) {
-                // nothing
-            }
-        });
-
-        menu.getSubmit().addActionListener(e -> {
-            menu.setNbPayer(Integer.parseInt(menu.getText().getText()));
-            System.out.println(menu.getNbPayer());
-        });
-
     }
 
 }
